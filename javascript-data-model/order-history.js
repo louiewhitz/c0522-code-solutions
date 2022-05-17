5d982ca9c2e538d00dc199ac4d3e3d7a2c4c98d5
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-17 19:03:13 +0000

    JavaScript Data Models

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,24 @@
+var orderHistory = [{
+  deliveryDate: 'August 8, 2020',
+  items: [{ name: 'JavaScript for impatient Programmers', type: 'book' }]
+
+},
+
+{
+  deliveryDate: 'July 20th',
+  items: [{ name: 'The Timeless way of building', type: 'book' }]
+
+},
+
+{
+  deliveryDate: 'July 7th',
+  items: [{ name: 'GameCubeAdaptor', type: 'adaptor' }]
+},
+
+{
+  deliveryDate: 'July 5th',
+  items: [[{ name: 'GameCube', type: 'controller' }], [{ name: 'The Art of Sql', type: 'book' }]]
+}
+
+];
+console.log(orderHistory);
