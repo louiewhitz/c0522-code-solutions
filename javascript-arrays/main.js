8c8e0d87c157ddbf7212b4ef68f13282895bdbcc
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-17 17:16:35 +0000

    Javascript-arrays

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -7,3 +7,6 @@ console.log('America is ' + colors[0] + ', ' + colors[1] + ' and ' + colors[2] +
 colors[2] = 'green';
 console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ' and ' + colors[2] + '.');
 console.log(colors);
+var students = ['Jake', 'Jessica', 'Autumn', 'Julian'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in this class');
