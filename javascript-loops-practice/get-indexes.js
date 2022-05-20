080ce2d25be4c11c5c126da8fc3dcd03e82c7742
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-20 05:59:09 +0000

    loops practice -not done

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,1 +1,8 @@
 /* exported getIndexes */
+function getIndexes(array) {
+  var getIndex = [];
+  for (var i = 0; i <= array.length; i++) {
+    array.split(array[i]);
+  }
+  return getIndex;
+}
--- javascript-loops-practice/sum-all.js
+++ javascript-loops-practice/sum-all.js
@@ -1,1 +1,9 @@
 /* exported sumAll */
+function sumAll(numbers) {
+  var myArray = [];
+  var sum = 0;
+  for (var i = sum; i < myArray.length; i++) {
+    sum += myArray[i];
+  }
+  return sum;
+}
