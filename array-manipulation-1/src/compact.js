21575a3e686b5aa0af7245bb9d2a6225f972a5d3
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-22 18:54:56 +0000

    array man 1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,11 +1,36 @@
 /* exported compact */
 function compact(input) {
+  // debugger;
   var newArr = [];
-  var truthy = [false, 0, '', null, undefined, NaN];
-  for (var j = 0; j < truthy.length; j++) {
-    if (input !== !!false || input !== '' || input[j] !== null || input[j] !== undefined || input[j] !== !!NaN) {
-      newArr.push([j]);
+  for (var j = 0; j < input.length; j++) {
+    if (input[j] === false) {
+      continue;
     }
+
+    if (input[j] === null) {
+      continue;
+    }
+
+    if (input[j] === '') {
+      continue;
+    }
+
+    if (input[j] === !!-0 || input[j] === 0) {
+      continue;
+    }
+
+    if (input[j] === undefined) {
+      continue;
+    }
+
+    if (isNaN(input[j]) && typeof input[j] !== 'string' && typeof input[j] !== 'object') { // will skip {} and 'fsad'
+      continue;
+    }
+
+    newArr.push(input[j]);
+
+    // if (input[j] !== false && input[j] !== '' && input[j] !== null && input[j] !== undefined && !isNaN(input[j]) && input[j] !== !!-0 && input[j] !== 0) {
+    // }
   }
   return newArr;
 }
