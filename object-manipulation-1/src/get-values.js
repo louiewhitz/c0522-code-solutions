3f1f1727a2704ac200751eb7f07242375daeb75a
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-22 19:09:58 +0000

    obj man 1 final

--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,1 +1,8 @@
 /* exported getValues */
+function getValues(object) {
+  var arr1 = [];
+  for (var key in object) {
+    arr1.push(object[key]);
+  }
+  return arr1;
+}
--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,1 +1,8 @@
 /* exported toObject */
+function toObject(keyValuePair) {
+  var key = keyValuePair[0];
+  var value = keyValuePair[1];
+  var obj = {};
+  obj[key] = value;
+  return obj;
+}
