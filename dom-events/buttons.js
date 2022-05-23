c2806a97eea4e4a7256b677d005fb58d297a3b22
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-23 21:34:46 +0000

    DOM Events

---
+++ dom-events/buttons.js
@@ -0,0 +1,26 @@
+
+function handleClick(event) {
+  console.log('button clicked!');
+  console.log(event);
+  console.log(event.target);
+}
+
+var $btnClick = document.querySelector('.click-button');
+$btnClick.addEventListener('click', handleClick);
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+var $hover = document.querySelector('.hover-button');
+$hover.addEventListener('mouseover', handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+var $double = document.querySelector('.double-click-button');
+$double.addEventListener('dblclick', handleDoubleClick);
