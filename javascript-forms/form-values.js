22d1b26466e9f47a02173d682a38f4e115d3e80f
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-25 22:38:52 +0000

    JS forms draft

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,13 @@
+var $form = document.querySelector('#contact-form');
+
+function submit(event) {
+
+  console.log('Form Submitted!');
+
+}
+$form.addEventListener('submit', submit);
+
+// var $actions = document.querySelector('.form-actions')
+// var $nameValue = $form[0].input.name.textContent;
+// var $emailValue = $form[0].input.email.textContent;
+// var textValue = $form[0].textarea.message.textContent;
