60255c749a139d364d2b235c27cad985270e5c19
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-26 04:38:51 +0000

    type tutor ampersand no verify

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,47 @@
+var text = 'grumpy wizards make toxic brew';
+var cursor = 0;
+window.addEventListener('load', function () {
+  loadPhrase();
+});
+
+function loadPhrase() {
+  for (var i = 0; i < text.length; i++) {
+    var input = document.querySelector('#input');
+    var char = text.charAt(i);
+    var span = document.createElement('span');
+    span.innerHTML = char;
+    input.appendChild(span);
+  }
+}
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  if (cursor === text.length) {
+    if (confirm('Would you like to try again?')) {
+      startOver();
+    }
+  } else {
+    // var pressedKey = ` ${e.code}`;
+    var pressedKey = String.fromCharCode(e.keyCode);
+    pressedKey = pressedKey.toLowerCase();
+
+    var allSpan = document.querySelectorAll('#input span');
+    var cursorSpan = allSpan[cursor];
+    var expectedChar = text.charAt(cursor);
+    if (pressedKey === expectedChar) {
+      cursorSpan.className = 'correct';
+    } else {
+      cursorSpan.className = 'incorrect';
+    } cursor++;
+  }
+}
+
+function startOver() {
+  cursor = 0;
+  var allSpan = document.querySelectorAll('#input span');
+  for (var i = 0; i < allSpan.length; i++) {
+    var char = allSpan[i];
+    char.className = '';
+  }
+}
