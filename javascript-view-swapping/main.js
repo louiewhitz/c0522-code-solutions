0b82b82b7493ecfe179db6c870fbbb15370f6a7e
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-27 18:02:33 +0000

    JS viewSwapping draft

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,27 @@
+// var $tabContainer = document.querySelector('.tab-container');
+// var $tabNodelist = document.querySelectorAll('.tab');
+// var $tab = document.querySelector('.tab');
+// var $viewNodeList = document.querySelectorAll('.view');
+
+// function checkClick(event) {
+//   if (!event.target.matches('.tab')) {
+//     return;
+//     }
+//     for (var i = 0; i < $tabNodelist.length; i++) {
+//       var $activeTab = $tabNodelist[i];
+//       if ($activeTab[i] === event.target) {
+//      $activeTab[i].className = 'tab active';
+//     } else {
+//         $activeTab[i].className = 'tab';
+//     }
+//       var $viewName = event.target.getAttribute('data-view');
+//       for (var i = 0; i < $viewNodeList.length; i++) {
+//         if ($viewNodeList[i].getAttribute('data-view') !== $viewName) {
+//         $viewNodeList[i].className = 'view hidden';
+//     } else {
+//         $viewNodeList[i].className = 'view';
+//     }
+//   }
+// }
+
+// $tabContainer.addEventListener('click', checkClick);
