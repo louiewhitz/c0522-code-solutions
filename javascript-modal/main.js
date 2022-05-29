380f5d2fdcc9649c248fdeaff4adab7bfb40c24e
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-29 05:39:01 +0000

    modal draft

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,8 +1,11 @@
+var button1 = document.querySelector('.modal');
+var no = document.querySelector('.no-btn');
+
 function openDialogue(event) {
-  var confirm = document.querySelector('#confirm');
-  confirm.style.display = 'block';
-  var shadey = document.querySelector('#shade');
-  shadey.style.display = 'block';
+  var confirm = document.querySelector('.confirm'); // this used to be #confirm;
+  confirm.style.display = 'block'; // very confused on how I should take the styling out of this
+  var shadey = document.querySelector('.shade'); // this used to be #shade
+  shadey.style.display = 'block'; // here too
 }
 
 function closeDialogue(event) {
@@ -12,10 +15,12 @@ function closeDialogue(event) {
   shadey.style.display = 'none';
 
 }
-window.addEventListener('load', function () {
 
-  var button1 = document.querySelector('.modal');
-  button1.addEventListener('click', openDialogue);
-  var no = document.querySelector('.no-btn');
-  no.addEventListener('click', closeDialogue);
-}, false);
+button1.addEventListener('click', openDialogue);
+no.addEventListener('click', closeDialogue);
+// window.addEventListener('load', function () {
+
+//   button1.addEventListener('click', openDialogue);
+//   var no = document.querySelector('.no-btn');
+//   no.addEventListener('click', closeDialogue);
+// }, false);
