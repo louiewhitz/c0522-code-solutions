e5a70f6f1cf17e6624e3ae4f6b9fce45e25aeff7
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-30 05:41:11 +0000

    Obj Man 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -19,4 +19,9 @@ function omit(source, keys) {
 // agenda: this time we must omit the keys
 // 1, create an empty object
 // store the value of source keys in a variable
-// loop through the source
+// loop through the source keys now that you have it in a veriable
+// create a variable for a singular key from the sourcekeys list
+// if the keys list does not include that specific key
+// create a new variable that is called value, and set it equal to the source at key;
+// if that value does not equal undefined
+// push into the new object we created earlier with the index at key being assigned to the variable value
