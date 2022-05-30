f6a8bd89af8e499e31bf8933bac2a6f7655bc8ba
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-05-30 00:12:04 +0000

    JS Modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,25 +2,19 @@ var button1 = document.querySelector('.modal');
 var no = document.querySelector('.no-btn');
 
 function openDialogue(event) {
-  var confirm = document.querySelector('.confirm'); // this used to be #confirm;
-  confirm.style.display = 'block'; // very confused on how I should take the styling out of this
-  var shadey = document.querySelector('.shade'); // this used to be #shade
-  shadey.style.display = 'block'; // here too
+  var confirm = document.querySelector('#confirm'); // this used to be #confirm;
+  confirm.className = 'visible';
+  var shadey = document.querySelector('#shade'); // this used to be #shade
+  shadey.className = 'visible';
 }
 
 function closeDialogue(event) {
   var confirm = document.querySelector('#confirm');
-  confirm.style.display = 'none';
+  confirm.className = 'non-visible';
   var shadey = document.querySelector('#shade');
-  shadey.style.display = 'none';
+  shadey.className = 'non-visible';
 
 }
 
 button1.addEventListener('click', openDialogue);
 no.addEventListener('click', closeDialogue);
-// window.addEventListener('load', function () {
-
-//   button1.addEventListener('click', openDialogue);
-//   var no = document.querySelector('.no-btn');
-//   no.addEventListener('click', closeDialogue);
-// }, false);
