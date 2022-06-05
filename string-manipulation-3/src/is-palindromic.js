fdb8a77122cd05ab903545149699fa541ba8a492
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-06-05 19:08:31 +0000

    string-man-3 working on title case

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,15 +1,20 @@
 // /* exported isPalindromic */
 // function isPalindromic(string) {
-//   var strLength = string.length;
-//   for (var i = 0; i < string.length / 2; i++); {
-//     strLength = string[i];
-//     if (string[i] !== string[strlength - 1 - i]) {
+//   var allStr = string.replace(' ', '');
+//   var halfString = allStr.length;
+//   for (var i = 0; i < allStr.length / 2; i++) {
+//     if (allStr[i] !== allStr[halfString - 1 - i]) {
 //       return false;
-
+//     }
 //   }
-
+//   return true;
 // }
-
+//   var half = string.length;
+//   for (var i = 0; i < string.length / 2; i++); {
+//     var half = string[i];
+//   if (string[i] !== half[len - 1 - i]) {
+//     return false;
+//   } else if
 // Palindrome means that something reads backward and forward the same
 // get the length of what the word is
 // go through the word and and single out the ccharacters
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,24 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var word = string.split(' ');
+  var array = [];
+  var final = [];
+
+  for (var i = 0; i < word.length; i++) {
+    array.push(word[i].split(''));
+    array[i].reverse();
+    final.push(array[i].join(''));
+  }
+  return final.join(' ');
+
+}
+
+// for (var i = 0; i < string.length; i++) {
+//   var word = string[i];
+//   reversedWord.map(word.split(' ').reverse().join(''));
+// for (var i = 0; i < string.length; i++) {
+//   var newWord = string[i];
+
+//   var reversedWord = newWord.split('').reverse();
+
+// }
