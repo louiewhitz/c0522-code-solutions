352b4cbf7daacbcc5832029dba6be4a577fa2d46
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-06-05 02:03:02 +0000

    string man 3

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,17 @@
+// /* exported isPalindromic */
+// function isPalindromic(string) {
+//   var strLength = string.length;
+//   for (var i = 0; i < string.length / 2; i++); {
+//     strLength = string[i];
+//     if (string[i] !== string[strlength - 1 - i]) {
+//       return false;
+
+//   }
+
+// }
+
+// Palindrome means that something reads backward and forward the same
+// get the length of what the word is
+// go through the word and and single out the ccharacters
+// divide the word in half
+// check if the first and last character are the same
