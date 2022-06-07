ff5d7976de3b2e77381a9bf4de811f891e4bfbb7
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-06-07 03:16:04 +0000

    Obj 3 start

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,33 @@
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// var player1 = {
+//   name: 'joe',
+//   hand: []
+// };
+
+// var player2 = {
+//   name: 'joe',
+//   hand: []
+// };
+
+// var player3 = {
+//   name: 'joe',
+//   hand: []
+// };
+
+// var player4 = {
+//   name: 'joe',
+//   hand: []
+// };
+// var ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+// function findRankIndex(array) {
+//   for (var i = 0; i < ranks.length; i++) {
+//     var ranksIndex = ranks[i];
+//     var randomCard = ranks[Math.floor(Math.random() * ranks.length)];
+//     console.log(randomCard);
+//   }
+//   return ranksIndex;
+// }
+// findRankIndex(ranks);
+
+// var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
+// var cardDeck = [];
