65901a1d13cc7dc8e6bf7fc7f5c19360e03235a1
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-06-07 17:04:14 +0000

    Object man 3 - deck created

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,4 +1,4 @@
-// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
 // var player1 = {
 //   name: 'joe',
 //   hand: []
@@ -18,16 +18,41 @@
 //   name: 'joe',
 //   hand: []
 // };
-// var ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
+var ranks = ['Ace of ', '2 of ', '3 of ', '4 of ', '5 ', '6 ', '7 ', '8 ', '9 ', '10 ', 'Jack ', 'Queen ', 'King '];
+
 // function findRankIndex(array) {
-//   for (var i = 0; i < ranks.length; i++) {
+//   for (var rankCount = 0; rankCount < 13; rankCount++) {
 //     var ranksIndex = ranks[i];
-//     var randomCard = ranks[Math.floor(Math.random() * ranks.length)];
-//     console.log(randomCard);
 //   }
+
 //   return ranksIndex;
 // }
+
 // findRankIndex(ranks);
 
-// var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
-// var cardDeck = [];
+// var randomCard = ranks[Math.floor(Math.random() * ranks.length)];
+var cardDeck = [];
+var suit = ['Clubs', 'Diamond', 'Heart', 'Spade'];
+for (var suitCount = 0; suitCount < 4; suitCount++) {
+  for (var rankCount = 0; rankCount < 13; rankCount++) {
+    cardDeck.push(ranks[rankCount] + suit[suitCount]);
+    console.log(cardDeck);
+  }
+}
+
+// function indexSuit(suitArray) {
+//   for (var i = 0; i < suit.length; i++) {
+//     var suitIndex = suit[i];
+//   }
+//   return suitIndex;
+// }
+
+// var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+
+// A deck of cards has 52 cards
+// the card deck is divided by 4 which gives you 12 different ranks
+// the suits are placed on each individual number rank equally, which means each rank has four different suits
+// first, create 4 players, and give them a name and a hand (future hand)
+// then put all the ranks into an array
+// then put all the suits into an array
